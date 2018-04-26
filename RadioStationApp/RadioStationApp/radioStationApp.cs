d618ae8a01793debd0c178a5077d5f21b0c4a920
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2018-04-26 14:15:23 -0400

    Add files via upload

---
+++ RadioStationApp/RadioStationApp/radioStationApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioStationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "C:\\Users\\User\\Desktop\\internet-radio.com.playlist.m3u";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://us1.internet-radio.com/proxy/dougwsjf?mp=/;";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "C:\\Users\\User\\Desktop\\listen.asx";
+        }
+    }
+}
